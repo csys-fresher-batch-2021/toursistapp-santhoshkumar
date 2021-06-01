abce00ebdb6c8225b48c7e61a33e2ea54ca1f341
Santhosh Kumar B <santhoshb0899@gmail.com>
2021-06-01 16:03:42 +0530

    updated issues

--- src/main/java/in/santhosh/servlet/ContactUsAction.java
+++ src/main/java/in/santhosh/servlet/ContactUsAction.java
@@ -18,6 +18,11 @@ import in.santhosh.service.ContactDetail;
  */
 @WebServlet("/contactUsAction")
 public class ContactUsAction extends HttpServlet {
+	private static final String END_DATE = "&endDate=";
+	private static final String START_DATE = "&startDate=";
+	private static final String PACKAGE_PRICE = "&packagePrice=";
+	private static final String PACKAGE_NAME = "&packageName=";
+	private static final String NUMBER_OF_DAYS = "&NumberOfDays=";
 	private static final long serialVersionUID = 1L;
        @Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -35,20 +40,20 @@ public class ContactUsAction extends HttpServlet {
 			if(ContactDetail.contactDetail(contactDetails))
 			{
 				String infoMessage="Response sended successfully";
-				response.sendRedirect("BookingDetail.jsp?infoMessage="+infoMessage+"&packageName="+countryName+
-						"&packagePrice="+packagePrice+"&startDate="+startDate+"&endDate="+endDate+"&NumberOfDays="+numberOfDays);
+				response.sendRedirect("BookingDetail.jsp?infoMessage="+infoMessage+PACKAGE_NAME+countryName+
+						PACKAGE_PRICE+packagePrice+START_DATE+startDate+END_DATE+endDate+NUMBER_OF_DAYS+numberOfDays);
 				
 			}
 			else {
 				String errorMessage = "Invalid Details";
-				response.sendRedirect("BookingDetail.jsp?errorMessage="+errorMessage+"&packageName="+countryName+
-						"&packagePrice="+packagePrice+"&startDate="+startDate+"&endDate="+endDate+"&NumberOfDays="+numberOfDays);
+				response.sendRedirect("BookingDetail.jsp?errorMessage="+errorMessage+PACKAGE_NAME+countryName+
+						PACKAGE_PRICE+packagePrice+START_DATE+startDate+END_DATE+endDate+NUMBER_OF_DAYS+numberOfDays);
 				
 			}
 			}else { 
 				String existsMessage = "you have already responded";
-				response.sendRedirect("BookingDetail.jsp?existsMessage="+existsMessage+"&packageName="+countryName+
-						"&packagePrice="+packagePrice+"&startDate="+startDate+"&endDate="+endDate+"&NumberOfDays="+numberOfDays);
+				response.sendRedirect("BookingDetail.jsp?existsMessage="+existsMessage+PACKAGE_NAME+countryName+
+						PACKAGE_PRICE+packagePrice+START_DATE+startDate+END_DATE+endDate+NUMBER_OF_DAYS+numberOfDays);
 			}
 		}catch(ServiceException e){
 			String message = e.getMessage();
